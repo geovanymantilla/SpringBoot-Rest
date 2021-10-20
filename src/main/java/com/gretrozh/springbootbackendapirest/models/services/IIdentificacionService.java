ee294a65b5e6cd62c4df9c532871876ea351e7e2
eduardcantillo <eduard1998n@gmail.com>
2021-10-20 14:03:11 -0500

    se crea la entidad tipo identificacion para  los usuarios

---
+++ src/main/java/com/gretrozh/springbootbackendapirest/models/services/IIdentificacionService.java
@@ -0,0 +1,7 @@
+package com.gretrozh.springbootbackendapirest.models.services;
+
+import com.gretrozh.springbootbackendapirest.models.entity.Identificacion;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IIdentificacionService extends CrudRepository<Identificacion,Integer> {
+}
