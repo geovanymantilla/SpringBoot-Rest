b79e0a9830283f064e45dd5fcae8582d661a0f97
eduardcantillo <eduard1998n@gmail.com>
2021-10-20 13:47:21 -0500

    consulta en el el dao de cliente para encontrar a un usuario por el nombre y apellido

--- src/main/java/com/gretrozh/springbootbackendapirest/models/dao/IClienteDao.java
+++ src/main/java/com/gretrozh/springbootbackendapirest/models/dao/IClienteDao.java
@@ -1,9 +1,11 @@
 package com.gretrozh.springbootbackendapirest.models.dao;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.gretrozh.springbootbackendapirest.models.entity.Cliente;
 
 public interface IClienteDao extends CrudRepository<Cliente, Long>{
 
+    public Cliente findByNombreAndApellido(String nombre,String apellido);
 }
