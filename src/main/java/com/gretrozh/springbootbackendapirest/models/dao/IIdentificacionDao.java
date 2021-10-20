2b3f8cfd560099ec5e82e47a28b3eba81aeccc87
eduardcantillo <eduard1998n@gmail.com>
2021-10-20 14:14:03 -0500

    se cre la interface service y se acomoda de paquete el dao de identificacion

--- src/main/java/com/gretrozh/springbootbackendapirest/models/dao/IIdentificacionDao.java
+++ src/main/java/com/gretrozh/springbootbackendapirest/models/dao/IIdentificacionDao.java
@@ -1,8 +1,8 @@
-package com.gretrozh.springbootbackendapirest.models.services;
+package com.gretrozh.springbootbackendapirest.models.dao;
 
 import com.gretrozh.springbootbackendapirest.models.entity.Identificacion;
 import org.springframework.data.repository.CrudRepository;
 
 public interface IIdentificacionDao extends CrudRepository<Identificacion,Integer> {
-    
+
 }
