5448e6ed433d65d93121bf98bc901a85cddb912f
GregorioPM <jose.gre122@gmail.com>
2021-10-28 00:13:56 -0500

    Creando Entidad EnlacesDeInteres

---
+++ src/main/java/com/gretrozh/springbootbackendapirest/entities/EnlaceInteres.java
@@ -0,0 +1,34 @@
+package com.gretrozh.springbootbackendapirest.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="enlace_interes")
+public class EnlaceInteres implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id_enlace_interes")
+    private int idEnlaceInteres;
+
+
+    private String url;
+
+    public int getIdEnlaceInteres() {
+        return idEnlaceInteres;
+    }
+
+    public void setIdEnlaceInteres(int idEnlaceInteres) {
+        this.idEnlaceInteres = idEnlaceInteres;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
