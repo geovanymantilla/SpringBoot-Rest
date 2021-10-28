95e4b963ec17dac2bc93885e4502f570c2f9fc09
GregorioPM <jose.gre122@gmail.com>
2021-10-28 00:19:34 -0500

    Dao Enlaces De interes

---
+++ src/main/java/com/gretrozh/springbootbackendapirest/models/dao/IEnlacesInteres.java
@@ -0,0 +1,7 @@
+package com.gretrozh.springbootbackendapirest.models.dao;
+
+import com.gretrozh.springbootbackendapirest.entities.EnlaceInteres;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IEnlacesInteres extends CrudRepository<EnlaceInteres,Integer> {
+}
