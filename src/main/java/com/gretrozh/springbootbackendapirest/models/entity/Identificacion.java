51ab329102b25a5f6ed01cb37399d78ab4a05c80
geovanymantilla <geovanymantilla@gmail.com>
2021-10-29 23:19:53 -0500

    Update Identificacion.java
    Se crean getter and setter entidad identificacion

--- src/main/java/com/gretrozh/springbootbackendapirest/models/entity/Identificacion.java
+++ src/main/java/com/gretrozh/springbootbackendapirest/models/entity/Identificacion.java
@@ -10,5 +10,53 @@ public class Identificacion {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private int nombre;
+    private String nombre;
+    
+    private String apellido;
+    
+    private String cedula;
+    
+    private String direccion;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+    
+    
 }
